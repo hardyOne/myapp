e97e4fca82c4e59dee250c4ed6fabe009ee38587
Heng Liu <hliuxjtu@163.com>
2019-04-29 22:30:44 -0400

    add mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
 // mysql
 var mysql = require('mysql');
@@ -36,8 +37,8 @@ mysqlCon.connect(function (err) {
 
 var MongoClient = require('mongodb').MongoClient;
 var uri = "mongodb+srv://cc1607:bestfriend@cluster0-atpu3.mongodb.net/test?retryWrites=true";
-var client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(function(err, db) {
+var client = new MongoClient(uri, {useNewUrlParser: true});
+client.connect(function (err, db) {
     if (err) {
         console.log('mongodb fails');
     }
@@ -66,12 +67,12 @@ router.post('/mysql', function (req, res, next) {
 router.post('/redshift', function (req, res) {
     console.log('route mongodb');
     let sqlText = req.body.sqlText;
-    console.log(sqlText)
-    obj = JSON.parse(sqlText);
-    console.log(obj['cols'])
-    obj['cols']['_id'] = 0
-    // obj = {'databaseName': 'NCAA', 'collectionName':'Seasons'};
-    client.db(obj['databaseName']).collection(obj['collectionName']).find({},{projection:obj['cols']}).limit(obj['limit']).toArray(function(err, result) {
+    console.log(sqlText);
+    let obj = convert(sqlText);
+    console.log(obj);
+    console.log(obj['cols']);
+    // obj = {'databaseName': 'NCAA', 'collectionName':'Seasons','cols':{}};
+    client.db('NCAA').collection(obj['table_name']).find(obj['condition'], {projection: obj['cols']}).limit(obj['limit']).toArray(function (err, result) {
         if (err) {
             console.log('err occurs');
             res.send(err);
@@ -82,6 +83,52 @@ router.post('/redshift', function (req, res) {
 });
 
 module.exports = router;
+
+var convert = function (sql) {
+    // remove heading and tailing white spaces
+    var sql = sql.trim();
+    // remove tailing ;
+    var sql = sql.split(';')[0];
+    var select_strs = sql.split('select');
+    var after_select = select_strs[1].trim();
+    var from_strs = after_select.split('from');
+    var cols = from_strs[0].trim();
+    var tmp = cols.split(',');
+    cols = [];
+    tmp.forEach(function (element) {
+        cols.push(element.trim())
+    });
+    var cols_dict = {};
+    cols.forEach(function (element) {
+        cols_dict[element] = 1
+    });
+    cols_dict['_id'] = 0;
+    var after_from = from_strs[1].trim();
+    var where_strs = after_from.split('where');
+    var table = where_strs[0].trim();
+    if (where_strs.length > 1) {
+        var after_where = where_strs[1].trim();
+        var col_strs = after_where.split('=');
+        var col = col_strs[0].trim();
+        var value = col_strs[1].trim();
+        var condition = {};
+        condition[col] = value;
+        return {
+            'table_name': table,
+            'cols': cols_dict,
+            'condition': condition,
+            'limit': 100000
+        };
+    }
+    return {
+        'table_name': table,
+        'cols': cols_dict,
+        'limit': 100000,
+        'condition': {}
+    };
+};
+
+
 
 
 
